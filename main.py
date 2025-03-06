6c5d6bad8a7e1490245ddb0748f3f15d1392e232
Yassine Radi <yassinradiz10@gmail.com>
2025-03-06 04:00:44 +0100

    sss

---
+++ main.py
@@ -0,0 +1,30 @@
+import warnings
+import torch
+import RealESRGANer
+import numpy as np 
+from PIL import Image
+from BasicSR import RRDBNet # model itself
+model_path = 'RealESRGAN_x4plus.pth'
+state_dict = torch.load(model_path, map_location=torch.device('cpu'))['params_ema']
+model = RRDBNet (num_in_ch=3, num_out_ch=3, num_feat=64, num_block=23, num_grow_ch=32, scale=4)
+model.load_state_dict(state_dict, strict=True)
+
+
+upsampler = RealESRGANer(
+
+scale=4,
+
+model_path=model_path,
+
+model=model,
+
+tile=0, pre_pad=0, half=True
+)
+img = Image.open('ana.jpg') .convert('RGB')
+
+img =np.array(img)
+output,  _ = upsampler.enhance(img, outscale=4)
+
+output_img = Image.fromarray(output)
+output_img.save('output.png')
+
